ad9ef5e4071d838946774478024fe58493239b5c
OeunSochetra <oeunsochetra@gmail.com>
2023-07-26 17:24:56 +0700

    update

--- components/CategoryDetail.tsx
+++ components/CategoryDetail.tsx
@@ -11,12 +11,16 @@ interface CategoryProps {
   thumb: string;
 }
 
-const CategoryDetail: React.FC = () => {
+const CategoryDetail = () => {
   const [showItems, setShowItems] = useState(false);
 
   const handleShow = () => {
-    setShowItems((prev) => !prev);
+    setShowItems(true);
   };
+
+  const displayedItems = showItems
+    ? category
+    : category.filter((item) => item.id === "movie").slice(0, 6);
 
   return (
     <div>
@@ -24,12 +28,14 @@ const CategoryDetail: React.FC = () => {
       <Container>
         <div className="flex items-end justify-end">
           <p className=" text-red-500 text-xl font-[400]">
-            <button onClick={handleShow} className="flex gap-3">
-              View all{" "}
-              <span className="text-xl pt-1">
-                <HiArrowCircleRight></HiArrowCircleRight>
-              </span>
-            </button>
+            {!showItems && (
+              <button onClick={handleShow} className="flex gap-3">
+                View all{" "}
+                <span className="text-xl pt-1">
+                  <HiArrowCircleRight></HiArrowCircleRight>
+                </span>
+              </button>
+            )}
           </p>
         </div>
         <div
@@ -38,6 +44,28 @@ const CategoryDetail: React.FC = () => {
         >
           {category.map((item) => {
             if (item.id === "movie") {
+              return (
+                <div key={item.id}>
+                  <img
+                    src={item.thumb}
+                    alt={item.title}
+                    className="rounded-xl xl:w-[180px] ms:w-[170px] md:w-[170px] cursor-pointer
+                             bg-gray-300 bg-opacity-60 shadow-xl backdrop-blur-md text-center 
+                               transition-all duration-500 hover:scale-105 active:scale-95 active:rotate-1.7"
+                  />
+                  <p>{item.title}</p>
+                </div>
+              );
+            }
+          })}
+        </div>
+
+        <div
+          className="pt-[100px] grid grid-cols-2 md:grid-cols-4 sm:grid-cols-4 xl:grid-cols-6 
+                     xxl:grid-cols-6 text-[#fff] gap-4 md:gap-4 sm:gap-2 xl:gap-3"
+        >
+          {category.map((item) => {
+            if (item.id === "drama") {
               return (
                 <div key={item.id}>
                   <img
